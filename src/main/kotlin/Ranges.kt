0deb7106cc872096461a31a036a8042b3ee6042b
owokomoses <owokomose@gmail.com>
2023-04-27 15:23:03 +0300

    added files

--- src/main/kotlin/Ranges.kt
+++ src/main/kotlin/Ranges.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    print("Number Range")
+    print("Number Range:")
 
     for (num in 3 ..9){
         println(num)
     }
+
+    print("Number range:")
+
+    for (num2 in 10 .. 15)
+        println(num2)
+
+    print("Character Ranges:")
+
+    for (char in 'C'..'I')
+        println(char)
 }
