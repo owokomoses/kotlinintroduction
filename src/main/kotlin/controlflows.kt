5920e0854cf2ce74b889f46d4b56da9422c0a1f0
owokomoses <owokomose@gmail.com>
2023-04-27 15:49:50 +0300

    added files

---
+++ src/main/kotlin/controlflows.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    //if..else statement
+
+    var num=5
+    if (num>=7){
+        println("The number is less")
+    }else{
+        println("The number is greater or equal ")
+    }
+}
