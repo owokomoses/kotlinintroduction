f1d6bb1989ba14c4a0c2e2f056d530ba4562b473
owokomoses <owokomose@gmail.com>
2023-04-28 15:21:21 +0300

    added files

---
+++ src/main/kotlin/Assignment.kt
@@ -0,0 +1,11 @@
+//Write down a kotlin program to check if a number is positive or negative or zero using if.. else statement.
+
+fun main(args: Array<String>) {
+    var num=-1
+    if (num<1){
+        println("The number is negative or zero")
+    }else{
+        println("The number is positive")
+
+    }
+}
