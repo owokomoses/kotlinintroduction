43feca748bbf0996efaee259821e1e23e585544f
owokomoses <owokomose@gmail.com>
2023-04-29 11:03:12 +0300

    "bum bum chao"

---
+++ src/main/kotlin/Assignment 2.kt
@@ -0,0 +1,17 @@
+//using a when or control statement expression create a simple calculator.use the concepts of input output.
+
+
+fun main(args: Array<String>) {
+    val a=10
+    val b=20
+
+    val c=if(a>b)a else b
+
+    val char="admin"
+    val result=when (char){
+        "admin"->"User is admin"
+        "admin"->"User is admin"
+        "admin"->"User is admin"
+        "admin"->"User is admin"
+    }
+}
