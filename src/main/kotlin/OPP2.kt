7717cb45d0111497f59ffeb0699332d7f2db3ee4
owokomoses <owokomose@gmail.com>
2023-05-09 15:36:37 +0300

    added files

--- src/main/kotlin/OPP2.kt
+++ src/main/kotlin/OPP2.kt
@@ -6,8 +6,19 @@ class eMobilis{
     }
 }
 
+class Cars{
+    var num:Int=50
+
+    fun Count():Int{
+        return num+num
+    }
+}
+
 fun main(args: Array<String>) {
     //Create object
-    val Obj=eMobilis()
+    val Obj = eMobilis()
+    val Cnt = Cars()
     println(Obj.Hesabu())
+    println(Cnt.Count())
+
 }
