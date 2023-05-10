16c1de63fccae6e97eb4a313ad2f75eb756122ed
owokomoses <owokomose@gmail.com>
2023-05-10 14:57:30 +0300

    added files

--- src/main/kotlin/Constractor.kt
+++ src/main/kotlin/Constractor.kt
@@ -2,10 +2,15 @@ class Students(val name:String,val gender:String,val age:Int) {
 
 
 }
+class cars(val name:String,val colour:String){
+
+}
 fun main(args: Array<String>) {
     //This is an object
     val myObj=Students("Moses","Male",18)
     val mydtls=Students("bryiana","Female",17)
+    val mycar=cars("Mercedes","black")
     println("Student Name is ${myObj.name} and he is ${myObj.gender} of ${myObj.age} years old")
     println("Student Name is ${mydtls.name} and she is ${mydtls.gender} of ${mydtls.age} years old")
+    println("Car Name is ${mycar.name} and its ${mycar.colour} in colour")
 }
---
+++ src/main/kotlin/Constructor2.kt
@@ -0,0 +1,8 @@
+class mimi(val jina:String,val shule:String,val miaka:Int){
+
+}
+
+fun main(args: Array<String>) {
+   val myinfo=mimi("Moses","Jamhuri high school",18)
+    println("My name is ${myinfo.jina} i am a student in ${myinfo.shule} and i am ${myinfo.miaka} years of age")
+}
